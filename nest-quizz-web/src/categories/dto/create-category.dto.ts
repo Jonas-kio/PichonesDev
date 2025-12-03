4ce4a75139b6e7bbbe6bf8aba09a74c12b2bc885
Kevin <kevindboy96@gmail.com>
2025-12-02 20:55:38 -0400

    FEAT: validaciones en create-category

--- nest-quizz-web/src/categories/dto/create-category.dto.ts
+++ nest-quizz-web/src/categories/dto/create-category.dto.ts
@@ -1,14 +1,14 @@
-import { IsString, IsOptional, IsBoolean } from 'class-validator';
+import { IsString, MinLength, MaxLength, Matches } from 'class-validator';
 
 export class CreateCategoryDto {
-    @IsString()
-    nombreCategoria: string;
+    @IsString({ message: 'El nombre debe ser un texto.' })
+    @MinLength(3, { message: 'El nombre debe tener mínimo 3 caracteres.' })
+    @MaxLength(50, { message: 'El nombre no puede superar los 50 caracteres.' })
+    @Matches(/^[A-Za-zÁÉÍÓÚÑáéíóúñ\s]+$/, { message: 'El nombre solo puede contener letras y espacios.'})
+    name: string;
 
-    @IsString()
-    @IsOptional()
-    descripcion?: string;
-
-    @IsBoolean()
-    @IsOptional()
-    activo?: boolean;
+    @IsString({ message: 'La descripción debe ser un texto.' })
+    @MinLength(5, { message: 'La descripción debe tener mínimo 5 caracteres.' })
+    @MaxLength(200, { message: 'La descripción no puede superar 200 caracteres.' })
+    description: string;
 }
