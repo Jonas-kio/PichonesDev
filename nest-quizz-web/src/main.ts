76774a2cadfdeff80c99b4d7439a2c7f91c30f44
Kevin <kevindboy96@gmail.com>
2025-12-09 23:07:51 -0400

    FEAT:implementacion de http/2

--- nest-quizz-web/src/main.ts
+++ nest-quizz-web/src/main.ts
@@ -5,12 +5,14 @@ import * as fs from 'fs';
 
 async function bootstrap() {
   const httpsOptions = {
+    allowHTTP1: true, //permite que navegadores viejos usen HTTP1, pero activa HTTP/2 si es posible
     key: fs.readFileSync('cert/key.pem'),
     cert: fs.readFileSync('cert/cert.pem'),
   };
 
   const app = await NestFactory.create(AppModule, {
     httpsOptions,
+    http2: true, //Activa HTTP/2
   });
 
   app.useGlobalPipes(
@@ -21,6 +23,7 @@ async function bootstrap() {
   );
 
   await app.listen(process.env.PORT || 3000);
-  console.log('🚀 HTTPS corriendo en https://localhost:3000');
+  console.log('🚀 Servidor corriendo con HTTP/2 en https://localhost:3000');
 }
+
 bootstrap();
