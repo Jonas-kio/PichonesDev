7af47128e9301bc5616a4771ac26db8a3285676c
Kevin <kevindboy96@gmail.com>
2025-12-09 23:01:23 -0400

    FEAT: implementacion de https

--- nest-quizz-web/src/main.ts
+++ nest-quizz-web/src/main.ts
@@ -1,9 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as fs from 'fs';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {
+    key: fs.readFileSync('cert/key.pem'),
+    cert: fs.readFileSync('cert/cert.pem'),
+  };
+
+  const app = await NestFactory.create(AppModule, {
+    httpsOptions,
+  });
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,5 +21,6 @@ async function bootstrap() {
   );
 
   await app.listen(process.env.PORT || 3000);
+  console.log('🚀 HTTPS corriendo en https://localhost:3000');
 }
 bootstrap();
